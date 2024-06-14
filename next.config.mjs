9b420ed4e5306a4e20ffb8083f5a46ee85e0a2e2
priopambudi <priopambudi869@gmail.com>
2024-06-14 14:15:49 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? '/priopambudi.github.io/' : '',
-};
+const nextConfig = {};
 
 export default nextConfig;
