189d9b6f9387bf2452411acabc876d8d16b4a1c5
priopambudi <priopambudi869@gmail.com>
2024-06-16 06:11:57 +0700

    about section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import { cn } from '@/utils/cn';
 import { BackgroundGradientAnimation } from './BackgroundGradientAnimation';
 import { GlobeDemo } from './GridGlobe';
+import Lottie from 'react-lottie';
+import { useState } from 'react';
+import animationData from '@/data/confetti.json';
+import MagicButton from './MagicButton';
+import { IoCopyOutline } from 'react-icons/io5';
 
 export const BentoGrid = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
   return (
@@ -31,6 +38,14 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('priopambudi869@gmail.com');
+
+    setCopied(true);
+  };
+
   return (
     <div
       className={cn(
@@ -66,9 +81,61 @@ export const BentoGridItem = ({
             {description}
           </div>
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">{title}</div>
-        </div>
 
-        {id === 2 && <GlobeDemo />}
+          {id === 2 && <GlobeDemo />}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-4">
+                {['NodeJS', 'Python', 'SQL'].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 px-3 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-4">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+                {['MongoDB', 'NextJS', 'Typescript'].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 px-3 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData: animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: 'xMidYMid slice',
+                    },
+                  }}
+                />
+              </div>
+
+              <MagicButton
+                title={copied ? 'Email copied' : 'Copy my email'}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
