03bbcb0b5b409e71066c13221aa730297728fe0f
priopambudi <priopambudi869@gmail.com>
2024-06-17 21:55:43 +0700

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Clients from '@/components/Clients';
 import Experience from '@/components/Experience';
+import Footer from '@/components/Footer';
 import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import RecentProjects from '@/components/RecentProjects';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import { navItems } from '@/data';
-import Image from 'next/image';
-import { FaHome } from 'react-icons/fa';
 
 export default function Home() {
   return (
@@ -18,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
