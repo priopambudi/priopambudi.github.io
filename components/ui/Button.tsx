e1cf3af867c7075acb4db216a0be242bfc2286bd
priopambudi <priopambudi869@gmail.com>
2024-08-07 23:53:28 +0700

    update color btn skill

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Button = ({ children }: Props) => {
   return (
-    <button className="py-1 px-3 border rounded-md text-sm cursor-default hover:bg-foreground hover:text-secondary transition-all">
+    <button className="py-1 px-3 border rounded-md text-sm cursor-default bg-primary text-secondary hover:bg-primary/80 transition-all">
       {children}
     </button>
   );
