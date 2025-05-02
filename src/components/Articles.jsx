3e4b90f4dfe608eabc267f03148068284d4a85b6
priopambudi <priopambudi869@gmail.com>
2025-05-02 17:01:56 +0700

    update projects and articles

---
+++ src/components/Articles.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Reveal from "./Reveal";
+import data from "../data.json";
+
+const Articles = () => {
+  return (
+    <section id="hero">
+      <div className="container mx-auto xl:px-36 py-20">
+        <Reveal>
+          <h2 className="font-bold text-3xl">Latest Articles</h2>
+        </Reveal>
+        <div className="grid grid-cols-3 gap-6 mt-9">
+          {data.articles.map((article, index) => (
+            <Reveal key={article} delay={(index + 1) * 0.25} width="100%">
+              <a href="#" className="w-full d-block">
+                <div className="img-wrap h-[250px] overflow-hidden w-full rounded-tl-[1rem] rounded-tr-[1rem] flex">
+                  <img
+                    src={article.img}
+                    alt="Prio Pambudi"
+                    className="object-cover w-full"
+                  />
+                </div>
+                <div className="bg-[#f2f2f2] px-6 py-6 rounded-bl-[1rem] rounded-br-[1rem]">
+                  <h3 className="text-2xl font-bold mb-1">{article.title}</h3>
+                  <p className="text-ls mt-3 line-clamp-2 w-full max-w-full">
+                    {article.description}
+                  </p>
+                  <p className="bg-[#f2f2f2] rounded-sm w-max text-xs mt-3">
+                    {article.date}
+                  </p>
+                </div>
+              </a>
+            </Reveal>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Articles;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,79 +1,42 @@
 import React from "react";
 import Reveal from "./Reveal";
-
-import profile from "/port.jpg";
+import data from "../data.json";
 
 const Projects = () => {
   return (
     <section id="hero">
       <div className="container mx-auto xl:px-36 py-20">
         <Reveal>
-          <h2 className="font-bold text-3xl">Projects</h2>
+          <h2 className="font-bold text-3xl">Latest Projects</h2>
         </Reveal>
         <div className="grid grid-cols-3 gap-6 mt-9">
-          <Reveal>
-            <a href="#" className="w-full">
-              <div className="img-wrap h-[250px] overflow-hidden w-full rounded-[1rem]">
-                <img
-                  src={profile}
-                  alt="Prio Pambudi"
-                  className="object-cover w-full"
-                />
-              </div>
-              <h3 className="text-2xl font-bold mt-4 mb-1">Dolts</h3>
-              <p className="text-xs">
-                A lightweight library in your local terminal!
-              </p>
-              <ul className="text-[9px] mt-2 flex items-center gap-2 flex-wrap">
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">Node.js</li>
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">
-                  Filesystem
-                </li>
-              </ul>
-            </a>
-          </Reveal>
-          <Reveal delay={0.5}>
-            <a href="#" className="w-full">
-              <div className="img-wrap h-[250px] overflow-hidden w-full rounded-[1rem]">
-                <img
-                  src={profile}
-                  alt="Prio Pambudi"
-                  className="object-cover w-full"
-                />
-              </div>
-              <h3 className="text-2xl font-bold mt-4 mb-1">Dolts</h3>
-              <p className="text-xs">
-                A lightweight library in your local terminal!
-              </p>
-              <ul className="text-[9px] mt-2 flex items-center gap-2 flex-wrap">
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">Node.js</li>
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">
-                  Filesystem
-                </li>
-              </ul>
-            </a>
-          </Reveal>
-          <Reveal delay={0.75}>
-            <a href="#" className="w-full">
-              <div className="img-wrap h-[250px] overflow-hidden w-full rounded-[1rem]">
-                <img
-                  src={profile}
-                  alt="Prio Pambudi"
-                  className="object-cover w-full"
-                />
-              </div>
-              <h3 className="text-2xl font-bold mt-4 mb-1">Dolts</h3>
-              <p className="text-xs">
-                A lightweight library in your local terminal!
-              </p>
-              <ul className="text-[9px] mt-2 flex items-center gap-2 flex-wrap">
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">Node.js</li>
-                <li className="bg-[#f2f2f2] py-1 px-2 rounded-sm">
-                  Filesystem
-                </li>
-              </ul>
-            </a>
-          </Reveal>
+          {data.projects.map((project, index) => (
+            <Reveal key={project} delay={(index + 1) * 0.25} width="100%">
+              <a href="#" className="w-full d-block">
+                <div className="img-wrap h-[250px] overflow-hidden w-full rounded-[1rem] flex">
+                  <img
+                    src={project.img}
+                    alt="Prio Pambudi"
+                    className="object-cover w-full"
+                  />
+                </div>
+                <h3 className="text-2xl font-bold mt-4 mb-1">
+                  {project.title}
+                </h3>
+                <p className="text-xs">{project.description}</p>
+                <ul className="text-[9px] mt-2 flex items-center gap-2 flex-wrap">
+                  {project.tech.map((tech) => (
+                    <li
+                      className="bg-[#f2f2f2] py-1 px-2 rounded-sm"
+                      key={tech}
+                    >
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </a>
+            </Reveal>
+          ))}
         </div>
       </div>
     </section>
